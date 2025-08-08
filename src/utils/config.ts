d114df53fee020682dcc6bd378e5473c9ccfec64
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-08 15:18:57 +0300

    Create config.ts

---
+++ src/utils/config.ts
@@ -0,0 +1,16 @@
+export default {
+  siteTitle: "The Mwaniki's Report",
+  frontmatter: {
+    requiredFields: ['title', 'date', 'description'],
+    optionalFields: [
+      'author',
+      'image',
+      'imageAlt',
+      'imageCaption',
+      'category',
+      'tags',
+      'featured',
+      'draft'
+    ]
+  }
+}
