4ca5c89ced7670a178fc9de3d8da8c891e2ff5c9
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-08 10:10:25 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://jonathanmwaniki.co.ke',
-  integrations: [tailwind()],
+  output: 'static',
+  build: {
+    assets: 'assets'
+  },
   markdown: {
     shikiConfig: {
       theme: 'github-light',
       wrap: true
+    },
+    remarkPlugins: [],
+    rehypePlugins: []
+  },
+  vite: {
+    css: {
+      transformer: 'postcss'
     }
   }
 });
