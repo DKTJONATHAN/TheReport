451854644f56f4b046a9d09909553edb8af1eed7
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-08 13:38:43 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import remarkGfm from 'remark-gfm';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 
-// Make sure this path is correct
-import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
-
 export default defineConfig({
   site: 'https://jonathanmwaniki.co.ke',
   output: 'static',
@@ -23,8 +20,7 @@ export default defineConfig({
       langs: ['bash', 'json', 'javascript', 'typescript', 'html', 'css']
     },
     remarkPlugins: [
-      remarkGfm,
-      remarkReadingTime
+      remarkGfm
     ],
     rehypePlugins: [
       rehypeSlug,
@@ -48,14 +44,6 @@ export default defineConfig({
       cssMinify: true,
       minify: 'terser',
       chunkSizeWarningLimit: 1600
-    }
-  },
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp',
-      config: {
-        quality: 80
-      }
     }
   },
   integrations: [
