dbfa6f12cbda4218efa0ea3755ec0e27f483764e
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-16 04:36:21 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   site: 'https://www.jonathanmwaniki.co.ke',
-  output: 'hybrid',
-  adapter: vercel({
-    maxDuration: 60
-  }),
+  output: 'static',
+  adapter: vercel(),
   integrations: [
     mdx(),
     sitemap()
