34315f9aac6642e11ccf2dd3b199fc700772fab3
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-16 17:01:40 +0300

    Update env.js

--- api/env.js
+++ api/env.js
@@ -1,13 +1,34 @@
-export default function handler(req, res) {
-  const rawCreds = process.env.GOOGLE_CREDENTIALS;
-  
-  return res.json({
-    exists: !!rawCreds,
-    length: rawCreds ? rawCreds.length : 0,
-    first_char: rawCreds ? rawCreds[0] : null,
-    last_char: rawCreds ? rawCreds[rawCreds.length - 1] : null,
-    starts_with_brace: rawCreds ? rawCreds.startsWith('{') : false,
-    ends_with_brace: rawCreds ? rawCreds.endsWith('}') : false,
-    first_100_chars: rawCreds ? rawCreds.substring(0, 100) : null
-  });
+import { google } from 'googleapis';
+import { JWT } from 'google-auth-library';
+
+export default async function handler(req, res) {
+  try {
+    const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+    const auth = new JWT({
+      email: credentials.client_email,
+      key: credentials.private_key,
+      scopes: ['https://www.googleapis.com/auth/webmasters']
+    });
+
+    const searchconsole = google.searchconsole({ version: 'v1', auth });
+
+    // Try to list ALL sites this service account has access to
+    const sites = await searchconsole.sites.list();
+
+    return res.json({
+      success: true,
+      service_account: credentials.client_email,
+      accessible_sites: sites.data.siteEntry || [],
+      total_sites: sites.data.siteEntry?.length || 0,
+      raw_response: sites.data
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+      code: error.code,
+      service_account: JSON.parse(process.env.GOOGLE_CREDENTIALS).client_email
+    });
+  }
 }
