1aad6480594d99b1704cf099f23271f2075d4f7d
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-16 18:57:22 +0300

    Update env.js

--- api/env.js
+++ api/env.js
@@ -1,34 +1,34 @@
-import { google } from 'googleapis';
 import { JWT } from 'google-auth-library';
 
 export default async function handler(req, res) {
   try {
     const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+    
+    // Test basic JWT authentication without any Google APIs
     const auth = new JWT({
       email: credentials.client_email,
       key: credentials.private_key,
       scopes: ['https://www.googleapis.com/auth/webmasters']
     });
 
-    const searchconsole = google.searchconsole({ version: 'v1', auth });
-
-    // Try to list ALL sites this service account has access to
-    const sites = await searchconsole.sites.list();
+    // Try to get an access token
+    const accessToken = await auth.getAccessToken();
 
     return res.json({
       success: true,
+      message: 'Service account authentication works',
       service_account: credentials.client_email,
-      accessible_sites: sites.data.siteEntry || [],
-      total_sites: sites.data.siteEntry?.length || 0,
-      raw_response: sites.data
+      has_access_token: !!accessToken.token,
+      token_type: typeof accessToken.token,
+      scopes: ['https://www.googleapis.com/auth/webmasters']
     });
 
   } catch (error) {
     return res.status(500).json({
       success: false,
       error: error.message,
-      code: error.code,
-      service_account: JSON.parse(process.env.GOOGLE_CREDENTIALS).client_email
+      stack: error.stack,
+      step: 'basic_auth_test'
     });
   }
 }
