e88c65f1457302ebfd848077eda9c65557d93b66
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2026-03-13 09:01:06 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 import indexJumpIntegration from './integrations/indexjump.js';
 
 export default defineConfig({
   site: 'https://jonathanmwaniki.co.ke',
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel({
-    webAnalytics: { enabled: true },
-    isr: true,
-    runtime: 'nodejs22.x'
+    webAnalytics: { enabled: true }
   }),
 
   trailingSlash: 'never',
