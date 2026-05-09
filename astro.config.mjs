1927e622ebf2112bf2ba3e51f045dfd6f981a186
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2026-05-09 07:01:28 +0000

    chore: remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
 import indexJumpIntegration from './integrations/indexjump.js';
 
 export default defineConfig({
   site: 'https://jonathanmwaniki.co.ke',
-  output: 'hybrid',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
 
   trailingSlash: 'never',
   compressHTML: true,
